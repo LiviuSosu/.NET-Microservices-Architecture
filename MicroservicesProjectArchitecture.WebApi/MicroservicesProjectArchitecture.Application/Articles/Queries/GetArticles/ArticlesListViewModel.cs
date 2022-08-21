2762936efc79f06f5ef1a0eb390b2249ade6ab62
LiviuSosu <sosuliviu@gmail.com>
2022-08-21 11:53:02 +0200

    test

--- MicroservicesProjectArchitecture.WebApi/MicroservicesProjectArchitecture.Application/Articles/Queries/GetArticles/ArticlesListViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace MicroservicesProjectArchitecture.Application.Articles.Queries.GetArticles
-{
-    public class ArticlesListViewModel
-    {
-        public IList<ArticleLookupModel> Articles { get; set; }
-    }
-}
